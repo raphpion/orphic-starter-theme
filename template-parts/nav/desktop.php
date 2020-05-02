c9a77d3d9d68d0d7cd00f9e37f9b2325db02661b
Thierry R. Le Bourdais <thierryr@orphic.ca>
2020-05-01 22:21:57 -0400

    Added nav menus logic

--- template-parts/nav/desktop.php
+++ template-parts/nav/desktop.php
@@ -5,6 +5,19 @@ $imageAlt = get_post_meta($custom_logo_id, '_wp_attachment_image_alt', TRUE);
 ?>
 
 <nav class="os-nav os-hidden-small os-hidden-tiny">
-  <img src="<?php echo $imageUrl; ?>" alt="<?php echo $imageAlt; ?>" class="os-nav-logo" />
-nav desktop
+  <a href="<?php echo get_home_url() ?>" >
+    <img src="<?php echo $imageUrl; ?>" alt="<?php echo $imageAlt; ?>" class="os-nav-logo" />
+  </a>
+  <?php
+      $topMenuItems = get_nav_menu_items_by_location('menu-1');
+      if (is_array($topMenuItems) || is_object($topMenuItems)):
+      foreach($topMenuItems as $item):
+          $link = $item->url;
+          $title = $item->title;
+      ?>
+      <a href="<?php echo $link ?>" class="<?php echo get_active_class($item); ?>"><?php echo $title ?></a>
+      <?php
+      endforeach;
+    endif;
+      ?>
 </nav>
--- template-parts/nav/mobile.php
+++ template-parts/nav/mobile.php
@@ -5,6 +5,19 @@ $imageAlt = get_post_meta($custom_logo_id, '_wp_attachment_image_alt', TRUE);
 ?>
 
 <nav class="os-nav os-hidden-main os-hidden-medium">
-  <img src="<?php echo $imageUrl; ?>" alt="<?php echo $imageAlt; ?>" class="os-nav-logo" />
-nav mobile
+  <a href="<?php echo get_home_url() ?>" >
+    <img src="<?php echo $imageUrl; ?>" alt="<?php echo $imageAlt; ?>" class="os-nav-logo" />
+  </a>
+  <?php
+      $topMenuItems = get_nav_menu_items_by_location('menu-1');
+      if (is_array($topMenuItems) || is_object($topMenuItems)):
+      foreach($topMenuItems as $item):
+          $link = $item->url;
+          $title = $item->title;
+      ?>
+      <a href="<?php echo $link ?>" class="<?php echo get_active_class($item); ?>"><?php echo $title ?></a>
+      <?php
+      endforeach;
+    endif;
+      ?>
 </nav>
