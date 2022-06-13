502e5ef620850146427261c84e9d1c318aad2e78
Raphaël Pion <60582987+raphpion@users.noreply.github.com>
2022-06-13 16:00:39 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -190,7 +190,7 @@ function os_add_gutenberg_assets() {
 function add_file_types_to_uploads($file_types){
 
   $new_filetypes = array();
-  $new_filetypes['svg'] = 'image/svg';
+  $new_filetypes['svg'] = 'image/svg+xml';
   $file_types = array_merge($file_types, $new_filetypes );
 
   return $file_types;
