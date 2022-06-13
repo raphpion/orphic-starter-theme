9d6f0de26ceeed12f43def6f530ed4360f10ee29
Raphaël Pion <60582987+raphpion@users.noreply.github.com>
2022-06-13 15:44:48 -0400

    Add SVG support to theme

--- functions.php
+++ functions.php
@@ -185,6 +185,19 @@ function os_add_gutenberg_assets() {
 }
 
 /**
+ * Allow SVG File uploads
+ */
+function add_file_types_to_uploads($file_types){
+
+  $new_filetypes = array();
+  $new_filetypes['svg'] = 'image/svg';
+  $file_types = array_merge($file_types, $new_filetypes );
+
+  return $file_types;
+}
+add_action('upload_mimes', 'add_file_types_to_uploads');
+
+/**
  * Include ACF
  */
 // Define path and URL to the ACF plugin.
